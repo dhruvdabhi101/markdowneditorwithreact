c6c9ea7573f56c62a7ae0dee97f961daad06f6f3
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2022-07-03 00:01:46 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const [input,setInput] = useState("Welcome To Markdown");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        autoFocus
+        className="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <ReactMarkdown className='markdown'>{input }</ReactMarkdown>
     </div>
   );
-}
+};
 
-export default App;
+
+
+
